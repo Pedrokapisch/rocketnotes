5553cf2cd117ed6b827f31ebbe4fe0ea4d53c1e8
pedro <pedro-gugusto@hotmail.com>
2024-11-28 21:21:45 -0300

    integrated front-end to database with axios and cors

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,11 +1,39 @@
-import { Container, Form, Background } from './styles'
-import { FiMail, FiLock, FiUser } from 'react-icons/fi'
-import { Link } from 'react-router-dom'
+import { useState } from 'react';
+import { FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Link, useNavigate } from 'react-router-dom';
 
-import { Input } from '../../components/Input'
-import { Button } from '../../components/Button'
+import { api } from '../../services/api';
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
+
+import { Container, Form, Background } from './styles';
 
 export function Signup() {
+  const [ name, setName ] = useState("");
+  const [ email, setEmail] = useState("");
+  const [ password, setPassword ] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleSignUp() {
+    if(!name || !email || !password) {
+      return alert("Preencha todos os campos!");
+    };
+
+    api.post("/users", { name, email, password })
+     .then(() => {
+        alert("Conta criada com sucesso!");
+        navigate("/")
+      })
+     .catch(error => {
+        if(error.response) {
+          alert(error.response.data.message);
+        } else {
+          alert("não foi possível cadastrar")
+        }
+      });
+  }
+
   return (
     <Container>
 
@@ -21,21 +49,24 @@ export function Signup() {
           placeholder="Nome"
           type="text"
           icon={FiUser}
+          onChange={e => setName(e.target.value)}
         />
         
         <Input 
           placeholder="E-mail"
           type="text"
           icon={FiMail}
+          onChange={e => setEmail(e.target.value)}
         />
 
         <Input 
           placeholder="Senha"
           type="password"
           icon={FiLock}
+          onChange={e => setPassword(e.target.value)}
         />
 
-        <Button title="Cadastrar" />
+        <Button title="Cadastrar" onClick={handleSignUp}/>
         
         <Link to="/">
           Voltar para o login
